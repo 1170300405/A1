49af535c0b74f44e35c15121adec8096393ebb10
1170300405 <1151906899@qq.com>
2019-07-02 13:12:48 +0800

    clock

---
+++ clock/src/clock/Display.java
@@ -0,0 +1,44 @@
+package clock;
+
+
+public class Display {
+    private int limit = 0;
+    private int value = 0;
+    
+    public Display(int limit)
+    {
+        this.limit = limit;
+    }
+    public void increase()
+    {
+        value++;
+        if(value==limit)
+        {
+            value = 0;
+        }
+    }
+    public int getValue()
+    {
+        return value;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /*Display d = new Display (24);
+        for( ; ; )
+        {
+            d.increase();
+            System.out.println(d.getValue());
+        }*/
+        Display hour = new Display(24);
+        Display minute = new Display(60);
+        while (true) {
+            minute.increase();
+            if (minute.getValue() == 0) {
+                hour.increase();
+            }
+            System.out.printf("%02d:%02d\n", hour.getValue(), minute.getValue());
+        }
+    }
+
+}
